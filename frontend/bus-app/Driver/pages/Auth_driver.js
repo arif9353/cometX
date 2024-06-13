242ec0d00d9f51cc064d5874cd0ff08b5e05e701
Arya <128399498+aryas006@users.noreply.github.com>
2024-06-13 15:41:17 +0530

    basic pages added

---
+++ frontend/bus-app/Driver/pages/Auth_driver.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Auth_driver = () => {
+  return (
+    <View>
+      <Text>Auth_driver</Text>
+    </View>
+  );
+};
+
+export default Auth_driver;
